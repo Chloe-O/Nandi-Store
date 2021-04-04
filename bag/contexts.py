d75c7e62b4fee6892776c05cd73f2dcee9a6ed73
Chloe-O <chloeosbourne@yahoo.co.uk>
2021-04-04 07:52:33 +0000

    item_data changed to 'quantity' in bag context

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,13 +11,13 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, item_data in bag.items():
+    for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += item_data * product.price
-        product_count += item_data
+        total += quantity * product.price
+        product_count += quantity
         bag_items.append({
             'item_id': item_id,
-            'quantity': item_data,
+            'quantity': quantity,
             'product': product,
         })
 
