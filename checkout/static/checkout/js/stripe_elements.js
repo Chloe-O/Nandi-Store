dfd8f7a8b244e84f554116095c82c72a3ed0bfdd
Chloe-O <chloeosbourne@yahoo.co.uk>
2021-04-09 16:27:25 +0000

    styles added to stripe js file

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -10,6 +10,21 @@ var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
@@ -49,7 +64,7 @@ form.addEventListener('submit', function(ev) {
     };
     var url = '/checkout/cache_checkout_data/';
 
-    $.post(url, postData).done(function() {
+    $.post(url, postData).done(function () {
         stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: card,
@@ -98,6 +113,7 @@ form.addEventListener('submit', function(ev) {
             }
         });
     }).fail(function () {
+        // just reload the page, the error will be in django messages
         location.reload();
     })
 });
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,4 @@
-# Code used from Code Institute
+# Code used from Code Institute - Boutique Ado
 
 from django.http import HttpResponse
 from django.core.mail import send_mail
