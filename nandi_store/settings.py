bd626c096101c331839a6a03eba62a5c2f7a62b7
Chloe-O <chloeosbourne@yahoo.co.uk>
2021-04-10 16:02:10 +0000

    Deployment  - Secret Key removed and debug set

--- nandi_store/settings.py
+++ nandi_store/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5r6q3!d#i19vpj!&0but3o230=8o+wcb-0k_$nr*3+-f&jum69'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['nandi-store.herokuapp.com', 'localhost']
 
